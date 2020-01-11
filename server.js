02f64ca8fc3a034228b188427b98d9197170b0b0
unknown <jmslick@gmail.com>
2020-01-11 16:38:01 -0600

    43. Hash password and register route.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+// Connect Database
+connectDB();
 
-app.get('/', (req, res) => res.json({ msg: 'Contact keeper api' }));
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-// ROUTES
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
