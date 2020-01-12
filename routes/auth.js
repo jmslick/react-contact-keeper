b7b8af8d97bf8ad351f4953ee8697ad2f51cb1b0
unknown <jmslick@gmail.com>
2020-01-11 19:05:07 -0600

    45. Authenticate Route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const config = require('config');
 const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
+const User = require('../models/User');
+
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
@@ -16,8 +18,54 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Authenticate user and get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Valid email required').isEmail(),
+    check('password', 'Password required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      // Find user in mongoDb
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid credentials: email' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid credentials: password' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // Sign and generae a token
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000
+        },
+        // callback
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.messaage);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
